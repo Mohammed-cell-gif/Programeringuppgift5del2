80ef0480abf67269a0b883a0e88b01a4edad9157
Mohammed-cell-gif <moal08008@utb.vaxjo.se>
2025-09-24 11:41:38 +0200

    Programeringuppgift5del2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+Console.WriteLine("Medeltempraturen är " + Medel(temperatur));
+Medel(double[] a);
+double summa = 0
+for (int i = 0; i < length; i++)
+summa += args[int]
+return (summa / a.length);
