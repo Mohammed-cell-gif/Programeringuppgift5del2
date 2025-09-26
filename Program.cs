89e6890d3f7da585758d690d73ef255d9fdc3881
Mohammed-cell-gif <moal08008@utb.vaxjo.se>
2025-09-26 13:56:53 +0200

    Programeringuppgift5del2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
-Console.WriteLine("Medeltempraturen är " + Medel(temperatur));
-Medel(double[] a);
-double summa = 0
-for (int i = 0; i < length; i++)
-summa += args[int]
-return (summa / a.length);
+ double sum = 0;
+            double average = 0;
+            //Print initial array values on co
+            Console.WriteLine("Initial Array");
+            foreach (double a in temperatur) {
+                Console.WriteLine(a);  
+                sum += a;
+            }
+            average = sum / temperatur.Length;
+            Console.WriteLine("Average of array is {0:F2}",average);
+ 
+            Console.ReadLine();
